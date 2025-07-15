90fe351b548ee17a397661530c3a1c9963d9d44d
이준서 <henryjoon2005@gmail.com>
2025-07-15 09:44:33 +0900

    prob2 LAST complete

--- projects/power_calculator.py
+++ projects/power_calculator.py
@@ -5,15 +5,15 @@ def print_power(num,exp):
     print(val)
 
 def is_num(obj):
-    try:
-        float(obj)
+    try: # 예외처리 구문
+        float(obj) # 실수형으로 형변환 시도: 성공하면 숫자. 오류나면 숫자 아님
         return True
     except:
         print("Invalid number input.")
         return False
     
-def is_int(obj):
-    for i in range(len(obj)):
+def is_int(obj): # 정수 판별 함수
+    for i in range(len(obj)): 
         if not('0' <= obj[i] <= '9'):
             print("Invalid exponent input")
             return False
